b7b0c3de0af21e0b182aa8dedf42b9680097708e
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-04-20 13:25:15 +0530

    Command Params

--- cli/cli.ts
+++ cli/cli.ts
@@ -1,25 +1,53 @@
 import { argv } from "process";
+import { PepperMintException } from "../src/exceptions/exception";
 
 export class PepperMintCli {
     private readonly arguments:Array<string>;
     private readonly length:number;
+    private command:string;
 
     private position:number = 0;
 
     constructor(args?:Array<string>) {
         this.arguments = args == undefined ? argv : args
         this.length = this.arguments.length
+
+        if(this.length > 0){
+            this.command = this.arguments[0]
+            this.arguments = this.arguments.slice(1)
+            this.length = this.arguments.length
+
+            this.parseCommandArguments()
+        } else {
+            process.exit()
+        }
     }
 
     public parseCommandArguments = ():void | null => {
         let current:string | null = this.currentArgument()
+        let commandParams:Map<string, string> = new Map<string, string>();
         while(current != null){
-            if(current == "new"){
-                console.log(this.position)
+            if(!current.startsWith("--")){
+                const exception = new PepperMintException({
+                    message : `Invalid flag - ${current} ❎`,
+                    suggestion : "User -- in the beginning of the option"
+                }).throwException(true)
             }
+
+            const commandArguments = current.split("=")
+            if(commandArguments.length != 2){
+                const exception = new PepperMintException({
+                    message : "Expected assignment 😦",
+                    suggestion : "Try assigning values : --<flag>=<value>"
+                }).throwException(true)
+            }
+
+            commandArguments[0] = commandArguments[0].slice(2)
+            commandParams.set(commandArguments[0], commandArguments[1])
             this.position += 1
             current = this.currentArgument()
         }
+        console.log(commandParams)
     }
 
     private currentArgument = ():string | null => {
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { PepperMintCli } from "../cli/cli";
 
 const args:PepperMintCli = new PepperMintCli(process.argv.slice(2))
-args.parseCommandArguments()
+
