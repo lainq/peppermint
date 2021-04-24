3a637d7a549be2f6327ba9b4e0808bae3e118b5a
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-04-24 18:33:00 +0530

    null

---
+++ src/lang/none.ts
@@ -0,0 +1,22 @@
+
+export class PepperMintNull {
+    public readonly noneType:null = null;
+
+    constructor(){
+
+    }
+
+    /**
+     * @returns {PepperMintNull}
+     */
+    public static none = ():PepperMintNull => {
+        return new PepperMintNull()
+    }
+
+    /**
+     * @returns {PepperMintNull}
+     */
+    private static convert = (convertElement:any):PepperMintNull => {
+        return PepperMintNull.none()
+    }
+}
