e287fd134fbedece2493c7ce2d47bc4a69b20c5e
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-04-30 14:28:13 +0530

    Formatted the code

--- src/exceptions/runtime.ts
+++ src/exceptions/runtime.ts
@@ -1,16 +1,16 @@
-import { colours, throwColoredText } from "../../modules/colors";
+import {colours, throwColoredText} from '../../modules/colors';
 
 export class PepperMintRuntimeError {
-    private readonly message:string;
+  private readonly message: string;
 
-    constructor(readonly msg:string) {
-        this.message = msg;
-    }
+  constructor(readonly msg: string) {
+    this.message = msg;
+  }
 
-    public raiseException(file:string, line:number){
-        console.log(throwColoredText(colours.fg.red, this.message))
-        console.log(throwColoredText(colours.fg.red, `In ${file} at line ${line}`))
+  public raiseException(file: string, line: number) {
+    console.log(throwColoredText(colours.fg.red, this.message));
+    console.log(throwColoredText(colours.fg.red, `In ${file} at line ${line}`));
 
-        process.exit()
-    }
+    process.exit();
+  }
 }
--- src/util/collection.ts
+++ src/util/collection.ts
@@ -1,49 +1,55 @@
-import { TokenPosition, Tokens } from '../lang/lexer'
-import { PepperMintRuntimeError } from '../exceptions/runtime'
+import {TokenPosition, Tokens} from '../lang/lexer';
+import {PepperMintRuntimeError} from '../exceptions/runtime';
 
 interface FileData {
-    filename : string;
-    lineNumber : number
+  filename: string;
+  lineNumber: number;
 }
 
 interface VariableCollectionOptions {
-    variableName : string,
-    variableType : string;
-    variableValue : any;
-    position : TokenPosition
-    file:FileData
+  variableName: string;
+  variableType: string;
+  variableValue: any;
+  position: TokenPosition;
+  file: FileData;
 }
 
 export class VariableCollection {
-    private collection:Map<string, VariableCollectionOptions> = new Map<string, VariableCollectionOptions>([]);
-    private length:number = 0
+  private collection: Map<string, VariableCollectionOptions> = new Map<
+    string,
+    VariableCollectionOptions
+  >([]);
+  private length: number = 0;
 
-    public createNewAssignment = (name, variable:Tokens<any>, file:FileData) => {
-        const data:VariableCollectionOptions = {
-            variableName : name,
-            variableType : variable.type,
-            variableValue : variable.token,
-            position : variable.position,
-            file : file
-        }
-        const exists = this.collection.get(data.variableName) == undefined
-        if(exists) {
-            const error = new PepperMintRuntimeError(`${name} already exists`).raiseException(
-                file.filename, file.lineNumber
-            )
-
-        }
-
-        this.collection.set(name, data)
+  public createNewAssignment = (
+    name,
+    variable: Tokens<any>,
+    file: FileData
+  ) => {
+    const data: VariableCollectionOptions = {
+      variableName: name,
+      variableType: variable.type,
+      variableValue: variable.token,
+      position: variable.position,
+      file: file,
+    };
+    const exists = this.collection.get(data.variableName) == undefined;
+    if (exists) {
+      const error = new PepperMintRuntimeError(
+        `${name} already exists`
+      ).raiseException(file.filename, file.lineNumber);
     }
 
-    public getVariableValue(name:string, file:FileData){
-        const data = this.collection.get(name)
-        if(!data){
-            const exception = new PepperMintRuntimeError(
-                `${name} not defined`
-            ).raiseException(file.filename, file.lineNumber)
-        }
-        return data
+    this.collection.set(name, data);
+  };
+
+  public getVariableValue(name: string, file: FileData) {
+    const data = this.collection.get(name);
+    if (!data) {
+      const exception = new PepperMintRuntimeError(
+        `${name} not defined`
+      ).raiseException(file.filename, file.lineNumber);
     }
+    return data;
+  }
 }
