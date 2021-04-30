e3951d9c94ce33f988b4e8e145697a6e903392b5
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-04-30 14:25:16 +0530

    Completed the variable collection

--- src/util/collection.ts
+++ src/util/collection.ts
@@ -1,24 +1,49 @@
 import { TokenPosition, Tokens } from '../lang/lexer'
+import { PepperMintRuntimeError } from '../exceptions/runtime'
 
+interface FileData {
+    filename : string;
+    lineNumber : number
+}
 
 interface VariableCollectionOptions {
     variableName : string,
     variableType : string;
     variableValue : any;
     position : TokenPosition
+    file:FileData
 }
 
 export class VariableCollection {
     private collection:Map<string, VariableCollectionOptions> = new Map<string, VariableCollectionOptions>([]);
     private length:number = 0
 
-    public createNewAssignment = (name, variable:Tokens<any>) => {
+    public createNewAssignment = (name, variable:Tokens<any>, file:FileData) => {
         const data:VariableCollectionOptions = {
             variableName : name,
             variableType : variable.type,
             variableValue : variable.token,
-            position : variable.position
+            position : variable.position,
+            file : file
         }
         const exists = this.collection.get(data.variableName) == undefined
+        if(exists) {
+            const error = new PepperMintRuntimeError(`${name} already exists`).raiseException(
+                file.filename, file.lineNumber
+            )
+
+        }
+
+        this.collection.set(name, data)
+    }
+
+    public getVariableValue(name:string, file:FileData){
+        const data = this.collection.get(name)
+        if(!data){
+            const exception = new PepperMintRuntimeError(
+                `${name} not defined`
+            ).raiseException(file.filename, file.lineNumber)
+        }
+        return data
     }
 }
